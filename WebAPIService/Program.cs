773f1b0c8acc8d92748e5a292632a0e73a35aa14
Tajerbashi <kamrantajerbashi@gmail.com>
2023-04-06 18:12:13 +0430

    Project completed and database is ready

--- WebAPIService/Program.cs
+++ WebAPIService/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<DatabaseContext>(options =>
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionStrings"));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 var app = builder.Build();
 

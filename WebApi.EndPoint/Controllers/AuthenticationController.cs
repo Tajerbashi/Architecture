d65c0b0034f4e5041c54dd6b4084f1927e5b1ceb
KTajerbsahi <kamrantajerbashi@gmail.com>
2023-11-26 23:19:36 +0330

    Feature : Create Authentication Controller

---
+++ WebApi.EndPoint/Controllers/AuthenticationController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.EndPoint.Models;
+
+namespace WebApi.EndPoint.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        public AuthenticationController() { }
+
+        public ResponseModel<bool> Login()
+        {
+            return new ResponseModel<bool>
+            {
+                Model = true,
+            };
+        }
+
+    }
+}
---
+++ WebApi.EndPoint/Models/ResponseModel.cs
@@ -0,0 +1,14 @@
+﻿using Application.Library.ModelBase.BaseDTOModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.EndPoint.Models
+{
+    public class ResponseModel<T> : IActionResult
+    {
+        public Task ExecuteResultAsync(ActionContext context)
+        {
+            return Task.CompletedTask;
+        }
+        public T Model { get; set; }
+    }
+}

93bdea41322154bf7b3fcdcca80b785733cb2de2
KTajerbsahi <kamrantajerbashi@gmail.com>
2023-11-27 02:21:01 +0330

    Feature : File Handler For Read and Write file in Project

---
+++ Documentation.Library/Extentions/FileHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Documentation.Library.Extentions
+{
+    public class FileHandler
+    {
+        public void StartFile(string name)
+        {
+            var path = "/Client";
+            var root = Path.GetTempFileName();
+            //System.Diagnostics.Process.Start(path);
+            Console.WriteLine($@"Path : {path}");
+            Console.WriteLine($@"root : {root}");
+        }
+    }
+}
--- Documentation.Library/Program.cs
+++ Documentation.Library/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Documentation.Library.Extentions;
+
+Console.WriteLine("StartProject");
+FileHandler handler = new FileHandler();
+handler.StartFile("");
